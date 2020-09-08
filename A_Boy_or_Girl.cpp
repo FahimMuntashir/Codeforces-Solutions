a21ecd07e8047d236c078838db9601bb015e873f
FahimMuntashir <fahim.tech.me@gmail.com>
2020-09-09 02:28:31 +0600

    boy and girl

---
+++ A_Boy_or_Girl.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+
+using namespace std;
+
+
+int main()
+{
+
+    string str;
+    int count =0;
+    cin>>str;
+    sort(str.begin(), str.end());
+
+    for(int i=0; i<str.size(); i++){
+        if (str[i]!=str[i+1])
+        {
+            count++;
+        }
+        
+    }
+
+    if (count%2==0)
+    {
+        cout<<"CHAT WITH HER!";
+
+    }else
+    {
+        cout<<"IGNORE HIM!";
+    }
+    
+    
+    
+}
