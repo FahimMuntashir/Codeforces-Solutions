f7bf275c5c51345a8addf39fceecd73febbdda73
FahimMuntashir <fahim.tech.me@gmail.com>
2020-09-14 00:37:03 +0600

    day 3

--- A_Panoramix_s_Prediction.c
+++ A_Panoramix_s_Prediction.c
@@ -1,28 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
-    int m,n;
+    int m, n;
+    int prime[] = { 2,  3,  5,  7,   11, 13,  17,  19,  23,  29, 31, 37, 41, 43, 47};
     scanf("%d%d", &m, &n);
     int count = 0;
 
-    for (int i = 2; i <n; i++)
+    for (int i = 0; i < 15; i++)
     {
-        if (i%n==0)
+        if (m==prime[i] && n== prime[i+1])
         {
-            count ++;
+            count++;
+            break;
         }
-        
-        
     }
-    if (count==0)
-    {
-        printf("YES\n");
-    }else
+    if (count == 0)
     {
         printf("NO\n");
     }
-    
-    
-    
+    else
+    {
+        printf("YES\n");
+    }
 }
