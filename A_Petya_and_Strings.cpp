cd23c8fbdf3a71193ce024a89cf3dcd085f4ce9e
FahimMuntashir <fahim.tech.me@gmail.com>
2021-02-16 22:33:23 +0600

    start again

--- A_Petya_and_Strings.cpp
+++ A_Petya_and_Strings.cpp
@@ -1,24 +1,48 @@
-#include <iostream>
-#include <algorithm>
+
+/*
+*
+* Author : Fahim Muntashir
+* Handle: f12r
+*
+*/
+
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
+#define f12r ios_base::sync_with_stdio(false), cin.tie(NULL)
+const double eps = 1e-9;
+#define MOD 1000000007
+#define pi acos(-1)
+#define ll long long
+#define endl "\n"
 
-    string str1, str2;
-    cin >> str1 >> str2;
-    for (int i = 0; i < str1.size(); i++) {
-        if (str1[i] < 92) {
-            str1[i] += 32;
-        }
-        if (str2[i] < 92) {
-            str2[i] += 32;
-        }
+/********** Main()  function **********/
+
+int main()
+{
+    f12r;
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    for (int i = 0; i < s1.size(); i++)
+    {
+        s1[i] = tolower(s1[i]);
+        s2[i] = tolower(s2[i]);
     }
-    if (str1 < str2) {
-        cout << -1;
-    } else if (str1 > str2) {
-        cout << 1;
-    } else if (str1 == str2) {
-        cout << 0;
+
+    if (s1 < s2)
+    {
+        cout << -1 << endl;
     }
+    else if (s1 == s2)
+    {
+        cout << 0 << endl;
+    }
+    else
+    {
+        cout << 1 << endl;
+    }
+
+    return 0;
 }
